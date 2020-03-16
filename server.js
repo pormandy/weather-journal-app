7ac345bc6b9491ffda817f7e7a5b1dc8f1182da8
Paul Ormandy <paul@punkmypixel.com>
2020-03-17 01:28:00 +0700

    first submission

---
+++ server.js
@@ -0,0 +1,44 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+/* Middleware*/
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Here we are configuring express to use body-parser as middle-ware.
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Spin up the server
+const port = 5000;
+
+// Callback to debug
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
+
+// GET route
+app.get('/data', (req, res) => {
+    console.log('GET request received');
+    res.send(projectData);
+});
+
+// POST route
+app.post('/', (req, res) => {
+    projectData.date = req.body.date;
+    projectData.temperature = req.body.main.temp;
+    projectData.feelings = req.body.feelings;
+    console.log('POST request received');
+    res.end();
+});
---
+++ website/app.js
@@ -0,0 +1,88 @@
+/* Global Variables */
+// US is default country. Parameter is zip code,country code
+const url = "http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=";
+const apiKey = "&APPID=d6bbb6da24f0e6d2203675681ff4867c";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// HTML Elements
+const divDate = document.getElementById('date');
+const divTemp =  document.getElementById('temp');
+const divContent =  document.getElementById('content');
+const divFeelings = document.getElementById('feelings');
+const divZip = document.getElementById('zip');
+
+
+// Get data using Fetch
+const getTheData = async (url = '') => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// POST data to body
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        
+        body: JSON.stringify(data)
+
+        });
+};
+
+// Update entryHolder
+const updateData = async () => {
+    const projectData = await getTheData('/data');
+    divDate.innerHTML = `${projectData.date}`;
+    divTemp.innerHTML = `${projectData.temperature} ℉`;
+    divContent.innerHTML = projectData.feelings;
+    console.log(projectData);
+};
+
+// Input data on button click
+const generateData = async () => {
+    const feelings = divFeelings.value;
+    const zip = divZip.value;
+    const response = await fetch(`${url}${zip}${apiKey}`);
+    try {
+        const data = await response.json();
+        data.feelings = feelings;
+        data.date = newDate;
+        await postData('/', data);
+        updateData();
+    } catch (error) {
+        console.error("error", error);
+    }
+};
+
+
+// Inial data
+const onLoadData = async () => {
+    const feelings = "Excited!";
+    const zip =10001;
+    const response = await fetch(`${url}${zip}${apiKey}`);
+    try {
+        const data = await response.json();
+        data.feelings = feelings;
+        data.date = newDate;
+        await postData('/', data);
+        updateData();
+    } catch (error) {
+        console.error("error", error);
+    }
+};
+
+document.getElementById('generate').addEventListener('click', generateData);
+
+onLoadData();
